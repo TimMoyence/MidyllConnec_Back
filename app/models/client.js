5fd06e1e0ae1a71b7983d4b788d06adcc4a30ba3
Tim Kraken <tim.moyence@gmail.com>
2024-09-27 09:10:11 +0200

    :hotfix:

--- app/models/client.js
+++ app/models/client.js
@@ -3,25 +3,25 @@ import pg from 'pg';
 
 const debug = Debug('mydillConnect:database');
 
+ const { Pool } = pg;
+
+ const client = new Pool({
+   connectionString: process.env.DATABASE_URL,
+ });
+
+ export default client;
+
 // const { Pool } = pg;
-
-// const client = new Pool({
-//   connectionString: process.env.DATABASE_URL,
+// 
+// const client = new Pool();
+// 
+// client.connect().then(() => {
+//   debug('database client connected');
 // });
-
-// export default client;
-
-const { Pool } = pg;
-
-const client = new Pool();
-
-client.connect().then(() => {
-  debug('database client connected');
-});
-
-export default {
-  originalClient: client,
-  async query(...params) {
-    return this.originalClient.query(...params);
-  },
-};
+// 
+// export default {
+//   originalClient: client,
+//   async query(...params) {
+//     return this.originalClient.query(...params);
+//   },
+// };
